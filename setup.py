a1d1dd527ad0ffaffabbf5073431b3e530a6867f
Magnus Olsson <magnus@minimum.se>
2018-12-12 21:14:35 +0100

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+
+version="2.0.1"
 setup(name='pypowermate',
-      version='2.0',
+      version=version,
       packages=['pypowermate'],
       install_requires=['evdev'],
       description='Encapsulates access to the Griffin USB PowerMate "knob" device',
       author='Magnus Olsson',
       author_email='magnus@minimum.se',
       url='https://github.com/blastur/pypowermate',
-      download_url='https://github.com/blastur/pypowermate/tarball/2.0',
+      download_url='https://github.com/blastur/pypowermate/tarball/{}'.format(version),
       keywords=['powermate', 'knob', 'griffin'],
       classifiers=[],
       )
